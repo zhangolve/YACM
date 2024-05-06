daad9b925b32289b0d8b6baaf5286b7ef0690b92
Olive Zhang <xingwang.zhang@everbridge.com>
2024-05-06 16:46:41 +0800

    update online status

--- src/app/commons/AccountCard.js
+++ src/app/commons/AccountCard.js
@@ -1,19 +1,28 @@
 
 import Table from '@/commons/Table'
-import { useWallet, networkToColor, useOnlineWallet } from '@/utils' 
+import { networkToColor, useMergedWallets, statusToColor } from '@/utils' 
+
 
 const ColoredNetwork = ({network}) => {
     return (
         <div class="h-2 w-2 rounded-full mr-2 inline-block" style={{background: networkToColor[network]?? 'black'}}></div>
     )
 }
-const columns = [{name: 'label'}, {name: 'address'},{name: 'network', render: (value)=><><ColoredNetwork network={value} /><span>{value}</span></>},{name: 'value'}]
+
+const ColoredStatus = ({online}) => {
+    return (
+        <div class="h-2 w-2 rounded-full mr-2 inline-block" style={{background: online ?statusToColor['Online']: 'gray'}}></div>
+    )
+}
+
+
+const columns = [{name: 'label'}, {name: 'address'},{name: 'network', render: (value)=><><ColoredNetwork network={value} /><span>{value}</span></>},{name: 'value'}, {name: 'online', render: (value)=><ColoredStatus online={value}/>}]
 
 
 
 const AccountCard = ()=> {
-    const [wallets, connectWallet] = useWallet()
-    const onlineWallet = useOnlineWallet()
+    const [wallets, connectWallet] = useMergedWallets()
+
     return (
     <div className="mx-auto bg-white rounded-xl shadow-md overflow-hidden card">
         <h2>Accounts</h2>
--- src/app/utils/index.js
+++ src/app/utils/index.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { ethers } from 'ethers'
 import useLocalStorage from "use-local-storage";
 import _ from 'lodash'
+import { net } from 'web3';
 
 
 export const useWallet = () => {
@@ -51,18 +52,17 @@ export const useWallet = () => {
 
 
 export const useOnlineWallet = () => {
-    const [wallet, setWallet] =  useState();
+    const [wallet, setWallet] =  useState({});
     useEffect(()=>{
         if(window.ethereum) {
             window.ethereum.on('accountsChanged', (accounts) => {
                 console.log(accounts,'accounts')
             });
             window.ethereum.on('chainChanged', (chainId, ...rest) => {
-                console.log(chainId,'chained',rest)
-                setWallet({...wallet, networkChainId: chainId})
+                setWallet({...wallet, networkChainId: parseInt(chainId,16)})
             });
         }
-    }, [])
+    }, [wallet])
 
     useEffect(()=>{
         async function getOnlineWallet() {
@@ -70,15 +70,56 @@ export const useOnlineWallet = () => {
             const signer = await provider.getSigner(); 
             const network = await provider.getNetwork()
             const networkName = network.name;
-            return {address: signer.address, network: networkName, }
+            const networkChainId = network.chainId;
+            return {address: signer.address, network: networkName, networkChainId}
         }
-        getOnlineWallet().then((wallet)=> {
-            setWallet(wallet)
-        })
+        
+        if (typeof window.ethereum !== 'undefined') {
+            console.log('MetaMask is installed!');
+            
+            // Check if MetaMask is connected to the website
+            window.ethereum
+                .request({ method: 'eth_accounts' })
+                .then((accounts) => {
+                    if (accounts.length > 0) {
+                        console.log('MetaMask is connected!');
+                        getOnlineWallet().then((wallet)=> {
+                            console.log(wallet, 'wallet')
+                            setWallet(wallet)
+                        })
+                        // MetaMask is connected, you can proceed with your application logic
+                    } else {
+                        console.log('MetaMask is not connected!');
+                    }
+                })
+                .catch((error) => {
+                    console.error('Error checking MetaMask connection:', error);
+                });
+        } else {
+            console.log('MetaMask is not installed!');
+        }
     }, [])
     return wallet
 }
 
+export const useMergedWallets = () => {
+    const [wallets, connectWallet] = useWallet([])
+    const onlineWallet = useOnlineWallet()
+    const [mergedWallets, setMergedWallets] = useState([])
+    useEffect(()=>{
+        if(onlineWallet) {
+            const result = wallets?.map((wallet)=>{
+                console.log(wallet, 'wallet', onlineWallet, 'onlineWallet',wallet.networkChainId, onlineWallet.networkChainId, wallet.networkChainId == onlineWallet.networkChainId, wallet.address === onlineWallet.address)
+                return {...wallet, online: wallet.address === onlineWallet.address && wallet.networkChainId == onlineWallet.networkChainId}
+            })
+            console.log(result,'result')
+            setMergedWallets(result??[])
+        } else {
+            setMergedWallets(wallets)
+        }
+    }, [onlineWallet,wallets])
+    return [mergedWallets, connectWallet]
+}
 
 export const networkToColor = {
     'mainnet': 'green',
@@ -87,4 +128,9 @@ export const networkToColor = {
     'kovan': 'purple',
     'goerli': 'orange',
     'bnb': 'yellow'
+}
+
+export const statusToColor = {
+    'Online': 'green',
+    'Offline': 'gray'
 }
